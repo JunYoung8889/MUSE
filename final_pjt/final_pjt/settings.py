1c9f56340f3fec227905913ecae73d78cda7847a
youngwoon Park <mkmkmk484848@gmail.com>
2022-05-25 17:01:24 +0900

    jyp2 community

--- final_pjt/final_pjt/settings.py
+++ final_pjt/final_pjt/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'movies',
+    'community',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
